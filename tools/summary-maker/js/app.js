c557bcf50f7c58d0218f8b5d4dcb13aefd6fef9c
Mariano Guerra <mariano@marianoguerra.org>
2023-10-15 17:11:25 +0200

    improvements

--- tools/summary-maker/js/app.js
+++ tools/summary-maker/js/app.js
@@ -3,42 +3,52 @@ const byId = (id) => document.getElementById(id);
 function main() {
   const input = byId("input"),
     output = byId("output"),
-    code = byId("code");
+    code = byId("code"),
+    withLine = byId("with-line");
 
   function update() {
-    updateOutput(input.value, output, code);
+    updateOutput(input.value, output, code, withLine.checked);
   }
 
   input.addEventListener("input", update);
+  withLine.addEventListener("input", update);
   update();
 }
 
-function updateOutput(s, output, codeNode) {
-  const code = format(s);
+function updateOutput(s, output, codeNode, withLine) {
+  const code = format(s, withLine);
 
   output.innerHTML = code;
   codeNode.textContent = code;
 }
 
 const rootClass = "my-summary-list",
-  prefix = `
+  suffix = `\n</div>`;
+
+function formatPrefix(opts) {
+  const bottomLine = opts?.withLine
+    ? ".my-summary-list details{padding-bottom:1em;border-bottom:1px solid rgb(114,130,127)}"
+    : "";
+  return `
 <div class="my-summary-list">
    <style type="text/css">
-    .my-summary-list details{margin-bottom:1em;}
+    .my-summary-list details{margin-bottom:1em;max-width:72ch}
     .my-summary-list p{margin-bottom:1em;margin-left:1em;}
+    .my-summary-list p:last-child{margin-bottom:0;}
     .my-summary-list summary, .my-summary-list p{
       font-family:futura-lt-w01-light,futura-lt-w05-light,sans-serif;
       color:rgb(114,130,127);
     }
+    ${bottomLine}
    </style>
-`,
-  suffix = `\n</div>`;
+`;
+}
 
 function isTitle(s) {
   return s[0] !== " " && s[0] !== "\t";
 }
 
-function format(s) {
+function format(s, withLine) {
   const groups = [],
     lines = s.trim().split("\n").filter((v) => !!v.trim());
 
@@ -64,7 +74,8 @@ function format(s) {
     groups.push(curGroup);
   }
 
-  return prefix + groups.map((g) => formatEntry(g.title, g.items)).join("\n") +
+  return formatPrefix({ withLine }) +
+    groups.map((g) => formatEntry(g.title, g.items)).join("\n") +
     suffix;
 }
 
